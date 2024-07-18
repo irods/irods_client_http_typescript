aa447d293aee0b2b73f2a603f5272350cecf5c5f
farbs03 <cgeraldfarber@icloud.com>
2024-07-18 15:21:25 -0400

    [#3] Remaining data object tests implemented - modify_replica and register
    Additional housekeeping done on resource test to delete child resource at the end of the tests.

--- src/tests/data_objects.test.ts
+++ src/tests/data_objects.test.ts
@@ -3,6 +3,7 @@ import type {
     IrodsResponse,
     URLComponentsType,
 } from '../types/general_types.js'
+import fs from 'fs'
 
 describe('DataObjectTests', () => {
     const urlComponents: URLComponentsType = {
@@ -11,7 +12,7 @@ describe('DataObjectTests', () => {
         version: '0.3.0',
     }
 
-    const api = new Wrapper(urlComponents, 'alice', 'alicepass')
+    const api = new Wrapper(urlComponents, 'rods', 'rods')
     let parallelWriteHandle: string | undefined
     let streamCount = 3
 
@@ -21,7 +22,7 @@ describe('DataObjectTests', () => {
 
     test('Touch a data object', async () => {
         const res = await api.data_objects.touch({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
@@ -32,7 +33,7 @@ describe('DataObjectTests', () => {
     test('Write to a data object', async () => {
         const testBuffer = Buffer.from('hello')
         const res = await api.data_objects.write({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
             bytes: testBuffer,
         })
         expect(res).toBeTruthy()
@@ -41,7 +42,7 @@ describe('DataObjectTests', () => {
 
     test('Read data object', async () => {
         const res = await api.data_objects.read({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
         })
         console.log(res)
         expect(res).toBeTruthy()
@@ -49,7 +50,7 @@ describe('DataObjectTests', () => {
 
     test('Parallel write init', async () => {
         const res = await api.data_objects.parallel_write_init({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
             'stream-count': streamCount,
         })
         parallelWriteHandle = res?.parallel_write_handle
@@ -66,7 +67,7 @@ describe('DataObjectTests', () => {
             testBuffer = Buffer.from(`hello${i}`)
             let res = api.data_objects.write({
                 bytes: testBuffer,
-                lpath: '/tempZone/home/alice/test.txt',
+                lpath: '/tempZone/home/rods/dataObject.txt',
                 'parallel-write-handle': parallelWriteHandle,
                 'stream-index': i,
                 offset: i * 6,
@@ -89,7 +90,7 @@ describe('DataObjectTests', () => {
 
     test('Modify metadata of a data object', async () => {
         const res = await api.data_objects.modify_metadata({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
             operations: [
                 {
                     operation: 'add',
@@ -105,8 +106,8 @@ describe('DataObjectTests', () => {
 
     test('Set permission of a data object', async () => {
         const res = await api.data_objects.set_permission({
-            lpath: '/tempZone/home/alice/test.txt',
-            'entity-name': 'rods',
+            lpath: '/tempZone/home/rods/dataObject.txt',
+            'entity-name': 'alice',
             permission: 'write',
         })
         expect(res).toBeTruthy()
@@ -115,11 +116,11 @@ describe('DataObjectTests', () => {
 
     test('Modify permissions of a data object', async () => {
         const res = await api.data_objects.modify_permissions({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
             operations: [
                 {
                     acl: 'read',
-                    entity_name: 'chris',
+                    entity_name: 'alice',
                 },
             ],
         })
@@ -129,7 +130,7 @@ describe('DataObjectTests', () => {
 
     test('Calculate checksum', async () => {
         const res = await api.data_objects.calculate_checksum({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
@@ -137,7 +138,7 @@ describe('DataObjectTests', () => {
 
     test('Verify checksum', async () => {
         const res = await api.data_objects.verify_checksum({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
@@ -145,7 +146,7 @@ describe('DataObjectTests', () => {
 
     test('Stat for data object', async () => {
         const res = await api.data_objects.stat({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
@@ -155,8 +156,8 @@ describe('DataObjectTests', () => {
 
     test('Copy data object', async () => {
         const res = await api.data_objects.copy({
-            'src-lpath': '/tempZone/home/alice/test.txt',
-            'dst-lpath': '/tempZone/home/alice/test2.txt',
+            'src-lpath': '/tempZone/home/rods/dataObject.txt',
+            'dst-lpath': '/tempZone/home/rods/dataObject2.txt',
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
@@ -164,8 +165,8 @@ describe('DataObjectTests', () => {
 
     test('Rename data object', async () => {
         const res = await api.data_objects.rename({
-            'old-lpath': '/tempZone/home/alice/test2.txt',
-            'new-lpath': '/tempZone/home/alice/testCopy.txt',
+            'old-lpath': '/tempZone/home/rods/dataObject2.txt',
+            'new-lpath': '/tempZone/home/rods/dataObjectCopy.txt',
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
@@ -174,7 +175,7 @@ describe('DataObjectTests', () => {
     //{ irods_response: { status_code: -1816000 } }
     test('Replicate data object', async () => {
         const res = await api.data_objects.replicate({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
             'src-resource': 'demoResc',
             'dst-resource': 'wrapperResc',
         })
@@ -184,52 +185,80 @@ describe('DataObjectTests', () => {
     })
 
     // Mutually exclusive fields of "replica-number" and "resource-hierarchy", at least 1 optional field must be filled
-    // { irods_response: { status_code: -13000 } } - don't have admin privileges - need to log in with rods credentials
-    // test('Modify data object replica', async () => {
-    //     const res = await api.data_objects.modify_replica({
-    //         lpath: '/tempZone/home/alice/test.txt',
-    //         'replica-number': 1,
-    //         'new-data-comments': 'test comment',
-    //     })
-    //     expect(res).toBeTruthy()
-    //     expect(res?.irods_response.status_code).toEqual(0)
-    // })
+    test('Modify data object replica', async () => {
+        const res = await api.data_objects.modify_replica({
+            lpath: '/tempZone/home/rods/dataObject.txt',
+            'replica-number': 1,
+            'new-data-comments': 'test comment',
+        })
+        expect(res).toBeTruthy()
+        expect(res?.irods_response.status_code).toEqual(0)
+    })
 
     test('Trim data object', async () => {
         const res = await api.data_objects.trim({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObject.txt',
             'replica-number': 1,
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
     })
 
-    // test('Register data object', async () => {
-    //     const res = await api.data_objects.register({
-    //         lpath: '/tempZone/home/alice/testCopy.txt',
-    //         ppath: 'uhhh',
-    //         resource: 'testResc',
-    //     })
-    //     expect(res).toBeTruthy()
-    //     expect(res?.irods_response.status_code).toEqual(0)
-    // })
+    test('Register data object', async () => {
+        // Need to create a local file
+        let filename = 'newly_registered_file.txt'
+        let physical_path = `src/tmp/${filename}`
+        let logical_path = `/tempZone/home/rods/${filename}`
+        let content = 'data'
+        try {
+            fs.writeFileSync(physical_path, content)
+        } catch (error) {
+            console.log('Error writing to file: ', error)
+        }
+
+        const res = await api.data_objects.register({
+            ppath: physical_path,
+            lpath: logical_path,
+            resource: 'demoResc',
+            'data-size': content.length,
+        })
+
+        try {
+            fs.rmSync(physical_path)
+        } catch (error) {
+            console.log('Error deleting file: ', error)
+        }
+
+        expect(res).toBeTruthy()
+        expect(res?.irods_response.status_code).toEqual(0)
+    })
 
     test('Remove data object', async () => {
         const res1 = await api.data_objects.remove({
-            lpath: '/tempZone/home/alice/testCopy.txt',
+            lpath: '/tempZone/home/rods/newly_registered_file.txt',
             'catalog-only': 0,
             'no-trash': 1,
         })
 
         const res2 = await api.data_objects.remove({
-            lpath: '/tempZone/home/alice/test.txt',
+            lpath: '/tempZone/home/rods/dataObjectCopy.txt',
+            'catalog-only': 0,
+            'no-trash': 1,
+        })
+
+        const res3 = await api.data_objects.remove({
+            lpath: '/tempZone/home/rods/dataObject.txt',
             'catalog-only': 0,
             'no-trash': 1,
         })
 
         expect(res1).toBeTruthy()
         expect(res1?.irods_response.status_code).toEqual(0)
+
         expect(res2).toBeTruthy()
         expect(res2?.irods_response.status_code).toEqual(0)
+
+        expect(res3).toBeTruthy()
+        expect(res3?.irods_response.status_code).toEqual(0)
     })
 })
