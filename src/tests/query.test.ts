6a1b8318da041dbc0ce4f6fb05ce233f13441673
farbs03 <cgeraldfarber@icloud.com>
2024-07-19 12:06:19 -0400

    [#2] Fixed execute_specific_query test

--- src/tests/query.test.ts
+++ src/tests/query.test.ts
@@ -3,8 +3,12 @@ import { getAPI } from './setupTests.js'
 describe('QueryTests', () => {
     const api = getAPI()
 
-    let query =
+    let genquery =
         "SELECT USER_NAME, USER_TYPE, USER_ZONE WHERE USER_TYPE != 'RODSGROUP'"
+
+    let specific_query =
+        "select token_id from R_TOKN_MAIN where token_name = 'rodsgroup'"
+    let specific_query_name = 'get_rodsgroup_token_id'
 
     beforeAll(async () => {
         await api.authenticate()
@@ -12,7 +16,7 @@ describe('QueryTests', () => {
 
     test('Execute genquery', async () => {
         const res = await api.queries.execute_genquery({
-            query: query,
+            query: genquery,
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
@@ -20,8 +24,8 @@ describe('QueryTests', () => {
 
     test('Add specific query', async () => {
         const res = await api.queries.add_specific_query({
-            name: 'list_users',
-            sql: query,
+            name: specific_query_name,
+            sql: specific_query,
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
@@ -29,16 +33,17 @@ describe('QueryTests', () => {
 
     test('Execute specific query', async () => {
         const res = await api.queries.execute_specific_query({
-            name: 'list_users',
+            name: specific_query_name,
         })
         console.log(res)
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
+        expect(res?.rows[0][0]).toEqual('200') // The token ID of rodsgroup is always 200.
     })
 
     test('Remove specific query', async () => {
         const res = await api.queries.remove_specific_query({
-            name: 'list_users',
+            name: specific_query_name,
         })
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
