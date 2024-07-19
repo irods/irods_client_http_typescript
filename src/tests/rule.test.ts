e373e180ef3031d6014d6e0f418f047e58c51773
farbs03 <cgeraldfarber@icloud.com>
2024-07-19 12:16:32 -0400

    [#8] Remaining rule tests implemented

--- src/tests/rule.test.ts
+++ src/tests/rule.test.ts
@@ -2,29 +2,55 @@ import { getAPI } from './setupTests'
 
 describe('RuleTests', () => {
     const api = getAPI()
+    let rep_instance = 'irods_rule_engine_plugin-irods_rule_language-instance'
 
     test('List Rule Engines', async () => {
         const res = await api.rules.list_rule_engines()
-        // console.log(res)
+        console.log(res)
         expect(res).toBeTruthy()
         expect(res?.irods_response.status_code).toEqual(0)
     })
 
-    // // recommended to include rep-instance field
-    // test('Execute rule', async () => {
-    //     const res = await api.rules.execute({
-    //         'rule-text': 'rule',
-    //         'rep-instance': 'idk',
-    //     })
-    //     expect(res).toBeTruthy()
-    //     expect(res?.irods_response.status_code).toEqual(0)
-    // })
+    // recommended to include rep-instance field
+    test('Execute rule', async () => {
+        let test_msg = 'This was run by the iRODS HTTP API test suite!'
+        const res = await api.rules.execute({
+            'rep-instance': rep_instance,
+            'rule-text':
+                'writeLine("stdout", "This was run by the iRODS HTTP API test suite!")',
+        })
+        console.log(res)
+        expect(res).toBeTruthy()
+        expect(res?.irods_response.status_code).toEqual(0)
+        expect(res?.stderr).toBeFalsy()
+        expect(res?.stdout).toEqual(test_msg + '\n')
+    })
 
-    // test('Remove delay rule', async () => {
-    //     const res = await api.rules.remove_delay_rule({
-    //         'rule-id': 0,
-    //     })
-    //     expect(res).toBeTruthy()
-    //     expect(res?.irods_response.status_code).toEqual(0)
-    // })
+    test('Remove delay rule', async () => {
+        // Schedule a delay rule to execute in the distant future.
+        const delayRes = await api.rules.execute({
+            'rep-instance': rep_instance,
+            'rule-text': `delay("<INST_NAME>${rep_instance}</INST_NAME><PLUSET>1h</PLUSET>") { writeLine("serverLog", "iRODS HTTP API"); }`,
+        })
+        expect(delayRes).toBeTruthy()
+        expect(delayRes?.irods_response.status_code).toEqual(0)
+
+        // Find the delay rule we just created.
+        // This query assumes the test suite is running on a system where no other delay
+        // rules are being created.
+        const findDelayRuleRes = await api.queries.execute_genquery({
+            query: 'select max(RULE_EXEC_ID)',
+        })
+        expect(findDelayRuleRes).toBeTruthy()
+        expect(findDelayRuleRes?.rows.length).toEqual(1)
+
+        // Remove delay rule
+        let ruleID: string | undefined = findDelayRuleRes?.rows[0][0]
+        if (!ruleID) return
+        const res = await api.rules.remove_delay_rule({
+            'rule-id': parseInt(ruleID),
+        })
+        expect(res).toBeTruthy()
+        expect(res?.irods_response.status_code).toEqual(0)
+    })
 })
