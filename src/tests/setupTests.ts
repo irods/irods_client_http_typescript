0eda3f31374280b4c10317335fa384276b037d3e
farbs03 <cgeraldfarber@icloud.com>
2024-07-23 11:33:57 -0400

    [#21] Added documentation to setupTests.ts

--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -10,10 +10,23 @@ const urlComponents: URLComponentsType = {
 const rodsadmin = new Wrapper(urlComponents, 'rods', 'rods')
 const rodsuser = new Wrapper(urlComponents, 'alice', 'alicepass')
 
+/*
+    As specified in jest.config.cjs, in the field "setupFilesAfterEnv",
+    this file will be ran before each test suite.
+    The beforeAll(...) below sets up both wrapper instances for usage within a test,
+    ensuring both are authenticated before tests begin running.
+*/
+
 beforeAll(async () => {
     await rodsadmin.authenticate()
     await rodsuser.authenticate()
 })
+
+/*
+    Allows wrapper instances to be imported into a test suite for usage.
+    The 'type' parameter is optional, and if not specified, the function returns the rodsadmin
+    instance by default.
+*/
 
 export function createClientForTesting(
     type?: 'rodsadmin' | 'rodsuser'
